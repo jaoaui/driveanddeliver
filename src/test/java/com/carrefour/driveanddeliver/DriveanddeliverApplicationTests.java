676a55bb631f8f3e824045eb44f4343810e9b6c4
Yassir JAOUAI <yassir.jaouai-prestataire@ca-ps.com>
2024-07-31 21:41:52 +0100

    #1 : feature : spring initalizer skeleton

---
+++ src/test/java/com/carrefour/driveanddeliver/DriveanddeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrefour.driveanddeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveanddeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
